917e7458c7c7e564cd60fa688fef440bfebe4690
Jack Chen <jjack1226tw@gmail.com>
2018-07-31 12:54:59 +1200

    Allow rotation for when mosue click event. Bugs like both mouse buttons being clicked fixed.

--- src/Client/App.js
+++ src/Client/App.js
@@ -1,9 +1,8 @@
 var player1;
-var targetX = 0;
-var targetY = 0;
+
 
 function startGame(){
-    player1 = new player(50,50,"red", 500,500);
+    player1 = new player(200,25,"red", 500,500);
     gameArea.start();
 }
 
@@ -11,7 +10,13 @@ function startGame(){
 var gameArea = {
     Canvas : document.getElementById('canvas'),
     Bounds : document.getElementById('canvas').getBoundingClientRect(),
+    targetX : screen.width/2,
+    targetY : screen.height/2,
+    lMousePress : 0, //1 when left mouse key pressed
+    rMousePress : 0, //1 when right mouse key pressed
+    mouseMove : 0, //1 for left rotate, 2 for right rotate, 0 for no rotate.
     start: function() {
+
         //setCanvasSize(this.bounds);
         document.getElementById('canvas').setAttribute('width', this.Bounds.width);
         document.getElementById('canvas').setAttribute('height', this.Bounds.height);
@@ -19,8 +24,28 @@ var gameArea = {
         this.interval = setInterval(updateGameArea, 15);
         canvas.addEventListener('mousemove', function(e) {
             var bounds = document.getElementById('canvas').getBoundingClientRect();
-            targetX = e.clientX - bounds.x;
-            targetY = e.clientY - bounds.y;
+            gameArea.targetX = e.clientX - bounds.x;
+            gameArea.targetY = e.clientY - bounds.y;
+        });
+        canvas.addEventListener('mousedown', function(e){
+            //e.button =  0 when left click, 2 when right click.
+            if(e.button == 0){
+                gameArea.lMousePress = 1;
+                gameArea.mouseMove  = 1;
+            }
+            else if(e.button == 2){
+                gameArea.rMousePress = 1;
+                gameArea.mouseMove = 2;
+            }
+        });
+        canvas.addEventListener('mouseup', function(e){
+            console.log(e.button);
+            if(e.button == 0) gameArea.lMousePress = 0;
+            else if(e.button == 2) gameArea.rMousePress = 0;
+            
+            if(!gameArea.lMousePress && !gameArea.rMousePress) {
+                gameArea.mouseMove = 0;
+            }
         });
 
     },
@@ -36,9 +61,9 @@ function player(width, height, color, x, y, type){
     this.color = color;
     this.x = x;
     this.y = y;
-    //this.angle = 0;
-    //this.moveAngle = 0;
-    //this.force = 5;
+    this.angle = 0;
+    this.moveAngle = 0;
+    this.force = 5;
     this.update = function(){
         var ctx = gameArea.ctx;
         ctx.save();
@@ -50,13 +75,13 @@ function player(width, height, color, x, y, type){
     };
     this.newPos = function(){
         this.angle += this.moveAngle * Math.PI / 180;
-        var position = velocity(this.x, this.y, targetX, targetY);
+        var position = velocity(this.x, this.y, gameArea.targetX, gameArea.targetY, this.force);
         this.x = this.x + position.velX;
         this.y = this.y + position.velY;    
     };
 }
 
-function velocity(x, y, targetX, targetY)
+function velocity(x, y, targetX, targetY, force)
 {
     var dx = targetX - x,
         dy = targetY - y,
@@ -72,7 +97,12 @@ function velocity(x, y, targetX, targetY)
 
 function updateGameArea(){
     gameArea.clear();
-    player1.moveAngle = 0;
+    //Left click
+    if(gameArea.mouseMove == 1) player1.moveAngle = 2;
+    //Right click
+    else if(gameArea.mouseMove == 2) player1.moveAngle = -2;
+
+    else player1.moveAngle = 0;
     player1.newPos();
     player1.update();
 }
